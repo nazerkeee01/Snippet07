1c128e423f8c0103a79303c389aa0f4ac2849e51
nazerkeee01 <65174393+nazerkeee01@users.noreply.github.com>
2021-01-03 21:35:51 +0600

    first commit

---
+++ cmd/web/handlers.go
@@ -0,0 +1,55 @@
+package main
+import (
+	fmt "fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+)
+
+type application
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		app.notFound(w)
+		return
+	}
+	files := []string{
+		"./ui/html/home.page.template",
+		"./ui/html/base.layout.template",
+		"./ui/html/footer.partial.template",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	err = ts.Execute(w, nil)
+	if err != nil {
+		app.serverError(w, err)
+	}
+}
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w) // Use the notFound() helper.
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Create a new snippet..."))
+}
+
+func (app *application) notFound(w http.ResponseWriter) {
+
+}
+
+func (app *application) clientError(w http.ResponseWriter, allowed int) {
+
+}
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module snippet07
+
+go 1.15
